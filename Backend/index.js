d0ced2d57dffb3f2c1b1a8a2c2f57cc3759a906c
Shubh Raj <147752429+Shubhraj54@users.noreply.github.com>
2025-02-12 20:46:27 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,1 @@
-import express from 'express'
-const PORT = 4000;
-const app = express();
-app.get('/',(req,res)=>{
-    res.send({message : 'connected'});
-})
-app.listen(PORT,()=>{
-    console.log(`localhost${PORT}`)
-})
+
