6ff11fdc80d88b3a51995348244840cf484cc05e
Shubh Raj <147752429+Shubhraj54@users.noreply.github.com>
2025-05-15 07:42:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
 
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const routes = require('./Router/routes.js');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
